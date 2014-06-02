96abb04b24fb7b0a9d9afd3c928ffebc8182e74d
bruno@triggolabs.com <bruno@triggolabs.com@621a468c-26a1-3c2b-13af-01ffc1995c69>
2014-06-02 16:39:37 +0000

    IGNORE - Refactoring code.

--- mediamanager-core/src/main/java/org/cruxframework/mediamanager/core/client/controller/ArtistControllerInterface.java
+++ mediamanager-core/src/main/java/org/cruxframework/mediamanager/core/client/controller/ArtistControllerInterface.java
@@ -4,11 +4,10 @@
 package org.cruxframework.mediamanager.core.client.controller;
 
 import org.cruxframework.mediamanager.core.client.dto.EditArtistDTO;
-import org.cruxframework.mediamanager.core.client.reuse.EditOperation;
 
-/**
+/**This interface provides methods that controllers
+ * implement to handle the return of database services
  * @author bruno.rafael
- *
  */
 public interface ArtistControllerInterface
 {
--- mediamanager-core/src/main/java/org/cruxframework/mediamanager/core/client/controller/EditControllerInterface.java
+++ mediamanager-core/src/main/java/org/cruxframework/mediamanager/core/client/controller/EditControllerInterface.java
@@ -2,7 +2,11 @@ package org.cruxframework.mediamanager.core.client.controller;
 
 import org.cruxframework.mediamanager.core.client.reuse.EditOperation;
 
+/**This interface provides methods that controllers
+ * implement to handle the return of database services
+ */
 public interface EditControllerInterface
 {
 	void completeInsert(EditOperation result);
+	void completeUpdate();
 }
